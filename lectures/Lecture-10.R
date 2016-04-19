41c11609d6b2224c8dd2486e6bf3a542ea7bb333
Matthew Aiello-Lammens <matt.lammens@gmail.com>
2016-04-19 18:22:38 -0400

    Updates to lecture 10 in class r notes

--- lectures/Lecture-10.R
+++ lectures/Lecture-10.R
@@ -47,3 +47,60 @@ anova(buckthorn_lm3, buckthorn_lm2)
 
 AIC(buckthorn_lm3)
 AIC(buckthorn_lm2)
+
+
+## Non-linear regression
+
+## Power-law example in Ecology - species area curve
+## Number of species = constant * Area^z
+
+## Number of invert species living in inter-tidal mussel clumps
+sar <- read.csv(file = "https://mlammens.github.io/Biostats/data/Logan_Examples/Chapter9/Data/peake.csv")
+
+## Look at these data
+head(sar)
+summary(sar)
+
+## Plot these data
+ggplot(data = sar, aes(x = AREA, y = SPECIES)) +
+  geom_point() + 
+  stat_smooth() +
+  theme_bw()
+
+## Fit a linear model
+sar_lm <- lm(data = sar, SPECIES ~ AREA)
+summary(sar_lm)
+plot(sar_lm)
+
+## Fit a non-linear model
+?nls
+sar_nls <- nls(data = sar, formula = SPECIES ~ const * AREA^z,
+               start = list(const = 0.1, z = 1) )
+summary(sar_nls)
+
+
+## Build an ANOVA
+## Diversity of diatoms based on the impact of ZINC. Diatoms that were found in RMNP.
+
+diatoms <- read.csv(file = "https://mlammens.github.io/Biostats/data/Logan_Examples/Chapter10/Data/medley.csv")
+
+head(diatoms)
+
+## Plot these data
+ggplot(data = diatoms, aes(x = ZINC, y = DIVERSITY)) +
+  geom_point() + 
+  theme_bw()
+
+ggplot(data = diatoms, aes(x = ZINC, y = DIVERSITY)) +
+  geom_boxplot() + 
+  theme_bw()
+
+## Running an ANOVA
+diatoms_lm <- lm(data = diatoms, DIVERSITY ~ ZINC)
+summary(diatoms_lm)
+
+anova(diatoms_lm)
+
+
+## Second way to do ANOVA
+summary(aov(data = diatoms, DIVERSITY ~ ZINC))
